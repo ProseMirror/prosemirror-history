e9e5caf70506f742258d46dfb0b86ac4c1a7721d
Bradley Ayers <bradley.ayers@gmail.com>
2017-07-12 16:14:02 +1000

    `newGroupDelay` is optional

--- src/history.js
+++ src/history.js
@@ -351,7 +351,7 @@ const closeHistoryKey = new PluginKey("closeHistory")
 //     The amount of history events that are collected before the
 //     oldest events are discarded. Defaults to 100.
 //
-//     newGroupDelay:: number
+//     newGroupDelay:: ?number
 //     The delay between changes after which a new group should be
 //     started. Defaults to 500 (milliseconds). Note that when changes
 //     aren't adjacent, a new group is always started.
