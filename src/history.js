ab0951c27445a2a154937e53c61ad87363b2d4ee
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-15 16:31:58 +0200

    Never split appended transactions into a new group
    FIX: Prevents appended transactions from starting a new event group,
    even if they were created a while after the previous transaction.
    
    Issue prosemirror/prosemirror#984

--- src/history.js
+++ src/history.js
@@ -280,8 +280,8 @@ function applyTransaction(history, state, tr, options) {
                               null, history.prevTime)
   } else if (tr.getMeta("addToHistory") !== false && !(appended && appended.getMeta("addToHistory") === false)) {
     // Group transforms that occur in quick succession into one event.
-    let newGroup = history.prevTime < (tr.time || 0) - options.newGroupDelay ||
-        !appended && !isAdjacentTo(tr, history.prevRanges)
+    let newGroup = !appended && (history.prevTime < (tr.time || 0) - options.newGroupDelay ||
+                                 !isAdjacentTo(tr, history.prevRanges))
     let prevRanges = appended ? mapRanges(history.prevRanges, tr.mapping) : rangesFor(tr.mapping.maps[tr.steps.length - 1])
     return new HistoryState(history.done.addTransform(tr, newGroup ? state.selection.getBookmark() : null,
                                                       options, mustPreserveItems(state)),
